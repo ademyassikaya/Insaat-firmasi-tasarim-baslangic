a204a4ae749def79714c494daa469123ecd4d321
ademyassikaya <34831128+ademyassikaya@users.noreply.github.com>
2023-07-30 22:47:08 +0300

    İnşaat firması başlangıç web site tasarımı
    Bootstrap ile tasarımı yapılıp php ile mail formu oluşturuldu

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Şirket ismi - İnşaat</title><!-- Değiştir -->
+        <meta charset="utf-8">
+        <meta name=”title” content=”Araba Kiralama” /><!-- Değiştir -->
+        <meta name="description" content="Araba kiralama ihtiyaçlarınıza kurumsal çözümler sunmaktayız." /><!-- Değiştir -->
+        <meta name="keywords" content="araba kiralama , araç kiralama , oto kiralama" /><!-- Değiştir -->
+        <meta name="robots" content="index, follow" />
+        <meta name=”author” content=”Savcı Group” /><!-- Değiştir -->
+        <meta name=”owner” content=”SavcıGroup” /><!-- Değiştir -->
+        <meta name=”copyright” content=”(c) 2018″ />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="images/logo2.ico"><!-- Değiştir -->
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="main.js"></script>
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+
+
+        <nav class="navbar">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="images/logo.svg" width="50" height="50" alt="Şirket ismi"><!-- Değiştir -->
+                </a>
+                <div class="uk-navbar-right">
+
+                    <ul class="uk-navbar-nav menu uk-visible@m">
+                        <li><a class="down" href="#araclar">HAKKIMIZDA</a></li>
+                        <li><a class="down" href="#sartlar">İŞLER</a></li>
+                        <li><a class="down" href="#hakkimizda">VİZYON / MİSYON</a></li>
+                        <li><a class="down" href="#iletisim">İLETİŞİM</a></li>
+                    </ul>
+
+                </div>
+                <a class="uk-hidden@m text-white" uk-toggle="target: #offcanvas-flip"><span uk-icon="icon: menu; ratio: 1.5"></span></a>
+            </div>
+
+            <div id="offcanvas-flip" uk-offcanvas="flip: true; overlay: true">
+                <div class="uk-offcanvas-bar">
+                <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+
+                    <h5 class="my-1">Şirket ismi  </h5> <!-- Değiştir -->
+
+                    <ul class="uk-nav uk-nav-default">
+                        <li><a class="down" href="#araclar">HAKKIMIZDA</a></li>
+                        <li><a class="down" href="#sartlar">İŞLER</a></li>
+                        <li><a class="down" href="#hakkimizda">VİZYON / MİSYON</a></li>
+                        <li><a class="down" href="#iletisim">İLETİŞİM</a></li>
+                    </ul>
+
+                </div>
+        </div>
+        </nav>
+
+       <section id="slider">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+  <img src="images/11.png" alt="sirket ismi">  <!-- Değiştir -->
+  <div class="carousel-caption d-none d-md-block">
+    <h5 class="text-white">BAŞLIK</h5><!-- Değiştir -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p><!-- Değiştir -->
+  </div>
+</div>
+    <div class="carousel-item">
+  <img src="images/22.png" alt="sirket ismi"> <!-- Değiştir -->
+  <div class="carousel-caption d-none d-md-block">
+    <h5 class="text-white">BAŞLIK</h5><!-- Değiştir -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p><!-- Değiştir -->
+  </div>
+</div>
+      </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+        </section>
+
+        <div class="arac" id="araclar">
+            <div class="container">
+
+                <h2 class="hakkimizda mt-5" style="text-align: center; font-family: avenir; font-size: 30px; font-weight: 200;">HAKKIMIZDA</h2>
+               <p class="uk-text-center@m">Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. </p><!-- Değiştir -->
+               <div class="uk-grid-small uk-child-width-expand@s uk-text-center hak" uk-grid>
+                    <div>
+                        <img src="images/1.png" width="50">
+                        <h5>Güven</h5>
+                        <p class="mt-3">Yapılarımızda güven açısından tüm araştırmalar yapılır ve güvenliginden emin olunana kadar tüm önlemler alınmaya devam eder.</p><!-- Değiştir -->
+                    </div>
+                    <div>
+                        <img src="images/2.png"  width="71">
+                         <h5>Kalite Standartları</h5>
+                        <p class="mt-3">Kaliteden kaçınmayıp her işimizde ve yapılarımızda kalitemizi ortaya koymaya devam edecegiz</p><!-- Değiştir -->
+                    </div>
+                    <div style="padding-bottom: 60px;">
+                        <img src="images/3.png"  width="50">
+                        <h5>Uygun Fiyat</h5>
+                        <p class="mt-3">Kaliteli ve uygun fiyatlı her cüzdana uygun yapılar.</p><!-- Değiştir -->
+                    </div>
+                </div>
+
+                <p class="pb-4" style="text-align: center; font-family: avenir; color: #41906F;"><i>- Doğru yatırımın tek adresi.</i> <b>Şirket İsmi</b></p><!-- Değiştir -->
+            </div>
+        </div>
+
+        <div class="sart" id="sartlar">
+            <div class="container polit">
+               <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+    <div class="uk-card-media-left uk-cover-container">
+        <img src="images/isler.jpg" alt="" uk-cover>
+        <canvas width="600" height="400"></canvas>
+    </div>
+    <div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">İNSAN KAYNAKLARI POLİTİKAMIZ</h3><!-- Değiştir -->
+            <p>İnsana ve çevreye saygılı, çalışanına ve müşterilerine değer veren, yeniliğe açık vizyonu ile kaynaklarını en etkin şekilde kullanmaya yönelik bir politika izliyoruz. Nitelikli işgücünü yaratmak ve yapıcı sonuçlarıyla müşterilerimize kaliteli hizmet sunmak.</p><!-- Değiştir -->
+        </div>
+    </div>
+</div>
+
+<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+    <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
+        <img src="images/isler2.jpg" alt="" uk-cover>
+        <canvas width="600" height="400"></canvas>
+    </div>
+    <div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">KALİTE POLİTİKAMIZ</h3><!-- Değiştir -->
+            <p>Müşterilerimizin kalite, fiyat, teslim süresi ve yüksek standartlardaki beklentilerini sorunsuz bir şekilde karşılamak. Zamanı iyi kullanıp, ölçülebilir ve ulaşılabilir hedefler doğrultusunda standart bir kalite yönetimi politikası yürütmek</p><!-- Değiştir -->
+        </div>
+    </div>
+</div>
+ <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+    <div class="uk-card-media-left uk-cover-container">
+        <img src="images/isler3.jpg" alt="" uk-cover>
+        <canvas width="600" height="400"></canvas>
+    </div>
+    <div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">İŞ SAĞLIĞI VE GÜVENLİĞİ POLİTİKAMIZ</h3><!-- Değiştir -->
+            <p>Çalışma ortamını, mesleki hastalıklar ve iş kazaları açısından minimum risk seviyesinde yapılandırmak. Çalışanların kendilerine teslim edilen iş güvenlik ekipmanların kullanmasını sağlamak ve bunun takibini süreklileştirmek. Çevreyi temiz ve düzenli tutarak sağlıklı çalışma ortamları yaratmaktır.</p><!-- Değiştir -->
+        </div>
+    </div>
+</div>
+            </div>
+        </div>
+
+        <div class="hakkimizda" id="hakkimizda">
+            <div class="container">
+                <h2 class="pt-4">MİSYON</h2>
+                <hr class="uk-divider-small" style="text-align: center; color: black;">
+                <p style="color: #4A4A4A; text-align: center;">Müşteri beklentilerini karşılayan veya müşteri beklentilerinin üstünde hizmet sunmaktır.   Güvenilir ve dürüst çalışma ilkeleriyle sektöründe aranılan isimlerden biri olarak nesiller boyu insanlığa hizmet verecek yapılar üretme bilinci içinde, yaptığı tüm projeleri kaliteli, sorunsuz ve zamanında yerine getirerek, en mükemmel sonuca ulaştırmak. Yaptığı işte en iyisini gerçekleştirdiğine dair inançlı ve hevesli olmak.</p><!-- Değiştir -->
+                <h2>VİZYON</h2>
+                <hr class="uk-divider-small" style="text-align: center; color: black;">
+                <p class="pb-5" style="color: #4A4A4A; text-align: center">Rekabetçi hizmet, yüksek kalite prensibi doğrultusunda her türlü özel kurum ve resmi kurumlar da dahil olmak üzere en geniş müşteri grubuna ulaşmaktır. Ürün ve hizmetlerimizde ahlaki kaliteyi en üstte tutarak prensiplerimizden asla ödün vermeden kullanışlı  ve yüksek kaliteli binalar ve yaşam alanları üretmek.
+Şirketimizin bütün maddi ve manevi kaynaklarını ve yeteneklerini, mükemmelliğe erişmek için sürekli canlı tutmak, tüm gücümüzü en iyi hizmet için seferber etmek. </p><!-- Değiştir -->
+            </div>
+        </div>
+
+        <div class="iletisim" id="iletisim">
+            <div class="container">
+                <h2>İLETİŞİM</h2>
+                <hr class="uk-divider-small" style="text-align: center; color: black;">
+
+                <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+                    <div class="my-3">
+
+                <?php
+                if(isset($_POST['ad']) && isset($_POST['eposta']) && isset($_POST['konu']) && isset($_POST['mesaj'])) {
+                    function epostakontrol($email){if (filter_var($email, FILTER_VALIDATE_EMAIL)){return 1;}
+                else {return 0;}}
+                    $gelen_mail = $_POST['eposta']; $mail_kontol=epostakontrol($gelen_mail);
+
+                if(empty($_POST['ad']) || empty($_POST['eposta']) || empty($_POST['konu']) || empty($_POST['mesaj'])) {
+                    echo '<div class="alert alert-danger" role="alert">
+                            Lütfen eksiksiz giriniz!
+                          </div>';
+                } else if($mail_kontol == "0"){echo 'Lütfen e-posta adresini doğru girin.';} else{
+                include 'class.phpmailer.php';
+                    $mail = new PHPMailer();
+                    $mail->IsSMTP();
+                    $mail->SMTPAuth = true;
+                    $mail->Host = 'mail.'; /*Mail yazılıcak*/
+                    $mail->Port = 587;
+
+                    $mail->Username = 'info@'; /* Değiştir mail yazılıcak şirket maili */
+                    $mail->Password = '';            /* Değiştir - Mail şirfesi */
+                    $mail->SetFrom($mail->Username, $_POST['ad']);
+                    $mail->AddAddress('', 'Ali Savcı'); /* Değiştir mail yazılıcak*/
+                    $mail->CharSet = 'UTF-8';
+                    $mail->Subject = $_POST["konu"];
+                    $mail->MsgHTML('İsim: '.$_POST["ad"].'<br/>
+                Konu: '.$_POST["konu"].'<br/>
+                E-Posta: '.$_POST["eposta"].'<br/>
+                Mesaj: '.$_POST["mesaj"].'<br/>');
+                if($mail->Send()) { echo '<div class="alert alert-success" role="alert">
+                        Mesajınız  gönderildi. Teşekkür ederiz.
+                </div>';}
+                else { echo 'Mesaj gönderirken bir hata oluştu ve girmiş olduğunuz bilgiler alınamadı.' . $mail->ErrorInfo;}
+                    }}
+                ?>
+
+                        <p>Bilgileri eksiksiz giriniz!</p>
+                        <form action="" method="post">
+                            <input type="text" class="form-control" id="formGroupExampleInput" name="ad" placeholder="İsim - Soyisim">
+                            <input type="number" class="form-control my-2" id="formGroupExampleInput" name="konu" placeholder="Telefon Numarası">
+                            <input type="email" class="form-control my-2" id="exampleInputEmail1" name="eposta" placeholder="e-mail">
+                            <textarea class="form-control" id="exampleFormControlTextarea1" name="mesaj" placeholder="Mesaj" rows="5"></textarea>
+                            <button type="submit" value="gonder" class="btn btn-dark my-2">GÖNDER</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <p>Şirket ismi &nbsp;&nbsp;| &nbsp;&nbsp;©2018 Tüm Hakları Saklıdır.</p><!-- Değiştir -->
+        </footer>
+
+    </body>
+</html>
